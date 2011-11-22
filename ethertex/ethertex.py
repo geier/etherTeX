c535f8cc128be4d1111448c269383778bebd4646
cg <geier at lostpackets d0t de>
2011-11-22 08:59:02 +0100

    using new pdf.js version, some minor cleaning up

--- ethertex/ethertex.py
+++ ethertex/ethertex.py
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python
 
-
 import cgi
 import cgitb; cgitb.enable()  # for troubleshooting
 
@@ -25,15 +24,25 @@ print """
     </head>
     <body>
 <div>
+<div style="float:left">
 <form>
 <button id="typeset">typeset</button>
 </form>
 </div>
+<div style="float:right">
+<p>
+<b>feel free to play around!</b>
+<a href="faq.html">FAQ</a>
+<a href="http://github.com/geier/ethertex">github</a>
+<a href="http://brutus.lostpackets.de/ethertex/pad.pdf" id="button-download">download pdf</a></p>
+</div>
 
+</div>
+<div style="clear:both;"></div>
 
 <iframe id="pdfframe" src="pdfjs.py" width=48% height=800px> 
 </iframe>
-<iframe id="etherframe" src="http://lostpads.de/p/testPad?showControls=true&showChat=true&showLineNumbers=true&useMonospaceFont=false" width=48% height=800px>
+<iframe id="etherframe" src="http://lostpads.de/p/testPad?showControls=false&showChat=true&showLineNumbers=true&useMonospaceFont=false" width=48% height=800px>
 </iframe>
 
     </body>
--- ethertex/pdfjs.py
+++ ethertex/pdfjs.py
@@ -1,23 +1,16 @@
 #!/usr/local/bin/python
-
-#EDIT
 pdflatex = "/usr/local/texlive/2011/bin/amd64-freebsd/pdflatex"
-
-import cgi
+import cgi 
 import cgitb; cgitb.enable()  # for troubleshooting
 import os
 import tempfile
 import codecs
 
-#EDIT
 from py_etherpad import EtherpadLiteClient
 myPad = EtherpadLiteClient('yourapitoken','http://etherpad.install.tld/api')
 
 #get the text of the etherpad
 padText = myPad.getText('testPad')
-
-
-
 
 tmpDir= tempfile.mkdtemp()
 
@@ -28,7 +21,6 @@ file = codecs.open("%s/pad.tex" %(tmpDir), "w", "utf-8")
 #print "%s/pad.tex" %(tmpDir)
 file.write(padText['text'])
 file.close()
-
 
 #cmdstring = "some_other_script.py %s %s" % (argument1 argument2)
 cmdstring = "%s -output-directory=%s -halt-on-error %s/pad.tex > /dev/null" %(pdflatex,tmpDir,tmpDir)
@@ -50,68 +42,19 @@ print """
         <title>Simple pdf.js page viewer</title>
         <link rel="stylesheet" href="viewer.css"></link>
 
-        <script type="text/javascript" src="compatibility.js"></script>
-        <script type="text/javascript" src="viewer.js"></script>
-        <script type="text/javascript" src="../pdf.js/pdf.js"></script>
+	  <script type="text/javascript" src="pdf.js"></script>
 
-        <script type="text/javascript" src="../pdf.js/fonts.js"></script>
-        <script type="text/javascript" src="../pdf.js/crypto.js"></script>
-        <script type="text/javascript" src="../pdf.js/glyphlist.js"></script>
+	  <script type="text/javascript">
+	    // Specify the main script used to create a new PDF.JS web worker.
+	    // In production, change this to point to the combined `pdf.js` file.
+	    PDFJS.workerSrc = 'pdf.js';
+	  </script>
+	  <script type="text/javascript" src="custom.js"></script>
   </head>
 
   <body>
-    <div id="controls">
-      <button id="previous" onclick="PDFView.page--;">
-
-        <img src="images/go-up.svg" align="top" height="32"/>
-        Previous
-      </button>
-
-      <button id="next" onclick="PDFView.page++;">
-        <img src="images/go-down.svg" align="top" height="32"/>
-        Next
-      </button>
-
-      <div class="separator"></div>
-
-      <input type="text" id="pageNumber" onchange="PDFView.page = this.value;" value="1" size="4"></input>
-
-      <span>/</span>
-      <span id="numPages">--</span>
-
-      <div class="separator"></div>
-
-      <select id="scaleSelect" onchange="PDFView.scale = parseInt(this.value);">
-        <option value="50">50%</option>
-
-        <option value="75">75%</option>
-        <option value="100">100%</option>
-        <option value="125">125%</option>
-        <option value="150" selected="selected"><p>150%</p></option>
-        <option value="200">200%</option>
-      </select>
-
-      <div class="separator"></div>
-
-      <input id="fileInput" type="file"></input>
-
-      <div class="separator"></div>
-
-      <span id="info">--</span>
-    </div>
-
-    <div id="sidebar">
-
-      <div id="sidebarBox">
-        <div id="sidebarScrollView">
-          <div id="sidebarView"></div>
-        </div>
-     </div>
-    </div>
-
-    <div id="viewer"></div>
+  <canvas id="the-canvas" style="border:1px solid black;"/>
   </body>
-
 </html>
 
 """
